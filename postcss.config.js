c5bb1204acc7ed5116db3612db1ed4be3b2ba507
stevengrc <junior_garcialagos@yahoo.com>
2025-10-27 18:30:37 -0600

    fix:change the pattern

--- postcss.config.js
+++ postcss.config.js
@@ -5,9 +5,9 @@ export default {
         './index.html',
         './src/**/*.{js,ts,jsx,tsx}',
       ],
-        safelist: {
-        greedy: [/.*/]
-      }
+        safelist: [
+        { pattern: /./ }
+      ]
     },
   },
 }
